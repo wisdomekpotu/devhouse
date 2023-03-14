3efd325148a35bf05f4a6873fbf18187fe92f864
Wisdom Ekpotu <whizfidel@gmail.com>
2023-03-14 14:55:03 +0100

    chore: added comments

--- src/context/UserAuthContext.js
+++ src/context/UserAuthContext.js
@@ -1,6 +1,14 @@
-import { createContext, useContext, useEffect, useState } from "react";
-import { onAuthStateChanged, signOut, GoogleAuthProvider, signInWithPopup, GithubAuthProvider, TwitterAuthProvider, FacebookAuthProvider } from "firebase/auth";
-import { auth } from "../firebase/firebase.utils";
+import { createContext, useContext, useEffect, useState } from 'react';
+import {
+  onAuthStateChanged,
+  signOut,
+  GoogleAuthProvider,
+  signInWithPopup,
+  GithubAuthProvider,
+  TwitterAuthProvider,
+  FacebookAuthProvider,
+} from 'firebase/auth';
+import { auth } from '../firebase/firebase.utils';
 
 const userAuthContext = createContext();
 
@@ -8,10 +16,10 @@ export function UserAuthContextProvider({ children }) {
   const [user, setUser] = useState({});
 
   function logOut() {
+    //signout
     return signOut(auth);
   }
 
-  
   function googleSignIn() {
     const googleAuthProvider = new GoogleAuthProvider();
     return signInWithPopup(auth, googleAuthProvider);
@@ -31,24 +39,28 @@ export function UserAuthContextProvider({ children }) {
     return signInWithPopup(auth, facebookAuthProvider);
   }
 
-
   useEffect(() => {
     const unsubscribe = onAuthStateChanged(auth, (currentuser) => {
-      console.log("Auth", currentuser);
+      console.log('Auth', currentuser);
       setUser(currentuser);
     });
 
     return () => {
       unsubscribe();
-
     };
   }, []);
 
-
-  //Will be accessible in application 
+  //Will be accessible in application
   return (
     <userAuthContext.Provider
-      value={{ user, logOut, googleSignIn, githubSignIn, twitterSignIn, facebookSignIn }}
+      value={{
+        user,
+        logOut,
+        googleSignIn,
+        githubSignIn,
+        twitterSignIn,
+        facebookSignIn,
+      }}
     >
       {children}
     </userAuthContext.Provider>
